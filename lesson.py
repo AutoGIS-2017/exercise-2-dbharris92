53b8b1130db02566424aff4e017964101a731cb5
dbharris92 <harris.db92@gmail.com>
2018-04-11 20:28:34 -0400

    initial commit

---
+++ lesson.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+AutoGIS lesson 2 geopandas
+This is a temporary script file.
+"""
+
+import geopandas as gpd
+
+# set filepath
+fp = r'F:\GS\harrisab2\S18\GeoViz\autoGIS_2\Data\DAMSELFISH_distributions.shp'
+
+# read file using gpd.read_file()
+data = gpd.read_file(fp)
+
+# write first 50 rows to a new shapefile
+out = r'F:\GS\harrisab2\S18\GeoViz\autoGIS_2\Data\DAMSELFISH_distributions_SELECTION.shp'
+
+# select first 50 rows
+selection = data[0:50]
+
+# write above rows into a new Shapefile (default output file format for geopandas)
+selection.to_file(out)
+
+# Select only specific columns with []
+data['geometry'].head()
+
+# make a selection of first five rows
+selection = data[0:5]
+
+# Iterate over the selected rows using .iterrows() MUST USE ITERROWS TO ITERATE OVER DATAFRAME
+for index, row in selection.iterrows():
+    # calculate area for row geometry only
+    poly_area = row['geometry'].area
+    # {0} and {1:.3f} are text formatting
+    # curly brackets take .format inputs and print them
+    print('Polygon area at index {0} is: {1:.2f}'.format(index, poly_area))
+    
+# create a new column of individual polygon areas
+selection['area'] = selection.area
+
+# find maximum, mean or minimum area
+#max_area = selection['area'].max()
+#mean_area = selection['area'].mean()
+#min_area = selection['area'].min()
+
+# make a geodataframe from scratch 
+# import necessary modules
+from shapely.geometry import Point, Polygon
+from fiona.crs import from_epsg
+
+# create empty geodataframe and geometry column
+newdata = gpd.GeoDataFrame()
+newdata['geometry'] = None
+
+# add coordinates and create polygon from coordinate-tuple list
+coordinates = [(24.950899, 60.169158), (24.953492, 60.169158), (24.953510, 60.170104), (24.950958, 60.169990)]
+poly = Polygon(coordinates)
+                
+# insert polygom data into geodataFrame using .loc
+newdata.loc[0, 'geometry'] = poly 
